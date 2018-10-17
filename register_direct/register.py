05a109393ec9c38dcb8903ad259c135709cf65c2
Theo Turner <theo@Theos-MacBook-Pro.local>
2018-10-17 19:12:10 +0100

    Code commenting and cleanup

--- register_direct/register.py
+++ register_direct/register.py
@@ -1,12 +1,15 @@
 from squid_py.ocean_contracts import OceanContracts
 from squid_py.consumer import register
 
-ocean = OceanContracts(host='http://localhost', port=8545, config_path='config.ini')
+# OceanContracts is the new name for OceanContractsWrapper
+ocean = OceanContracts(host = 'http://localhost',
+                       port = 8545,
+                       config_path = 'config.ini')
 
 # TODO: fill in JSON data as specified in contracts
 
-register(publisher_account=ocean.web3.eth.accounts[1],
-         provider_account=ocean.web3.eth.accounts[0],
-         price=10,
-         ocean_contracts_wrapper=ocean,
-         json_metadata=json_consume)
+register(publisher_account = ocean.web3.eth.accounts[1],
+         provider_account = ocean.web3.eth.accounts[0],
+         price = 10,
+         ocean_contracts_wrapper = ocean,
+         json_metadata = json_consume)
