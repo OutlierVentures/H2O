a93a81400ffb48fc5cacc92dc52171dc73b125e8
Theo Turner <theo@Theos-MacBook-Pro.local>
2018-10-17 19:05:28 +0100

    Add todo

--- register_direct/register.py
+++ register_direct/register.py
@@ -3,6 +3,8 @@ from squid_py.consumer import register
 
 ocean = OceanContracts(host='http://localhost', port=8545, config_path='config.ini')
 
+# TODO: fill in JSON data as specified in contracts
+
 register(publisher_account=ocean.web3.eth.accounts[1],
          provider_account=ocean.web3.eth.accounts[0],
          price=10,
