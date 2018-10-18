3b77b0e645ad8e9525c0ebb523012b6b51224ebf
Theo Turner <theo@Theos-MacBook-Pro.local>
2018-10-18 11:45:45 +0100

    Asset metadata object

--- register_direct/register.py
+++ register_direct/register.py
@@ -3,13 +3,52 @@ from squid_py.consumer import register
 
 import json
 
-# Will be posted through requests library
-test_object = {
-    # Required, check full ocean stack for format
-    # Will be web3.toHex-ed
-    "assetId": "",
-}
-test_consumable = json.dumps(test_object)
+
+
+
+
+json_dict = {"publisherId": "0x1",
+             "base": {
+                 "name": "UK Weather information 20111",
+                 "description": "Weather information of UK including temperature and humidity",
+                 "size": "3.1gb",
+                 "author": "Met Office",
+                 "license": "CC-BY",
+                 "copyrightHolder": "Met Office",
+                 "encoding": "UTF-8",
+                 "compression": "zip",
+                 "contentType": "text/csv",
+                 "workExample": "stationId,latitude,longitude,datetime,temperature,humidity\n"
+                                "423432fsd,51.509865,-0.118092,2011-01-01T10:55:11+00:00,7.2,68",
+                 "contentUrls": ["https://testocnfiles.blob.core.windows.net/testfiles/testzkp.pdf"],
+                 "links": [
+                     {"sample1": "http://data.ceda.ac.uk/badc/ukcp09/data/gridded-land-obs/gridded-land-obs-daily/"},
+                     {
+                         "sample2": "http://data.ceda.ac.uk/badc/ukcp09/data/gridded-land-obs/gridded-land-obs-averages-25km/"},
+                     {"fieldsDescription": "http://data.ceda.ac.uk/badc/ukcp09/"}
+                 ],
+                 "inLanguage": "en",
+                 "tags": "weather, uk, 2011, temperature, humidity",
+                 "price": 10,
+                 "type": "dataset"
+             },
+             "curation": {
+                 "rating": 0,
+                 "numVotes": 0,
+                 "schema": "Binary Votting"
+             },
+             "additionalInformation": {
+                 "updateFrecuency": "yearly"
+             },
+             "assetId": "001"
+             }
+
+
+
+
+
+
+test_consumable = json.dumps(json_dict)
 
 # OceanContracts is the new name for OceanContractsWrapper
 ocean = OceanContracts(host = 'http://localhost',
