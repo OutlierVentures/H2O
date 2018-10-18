25c3faec8e69f097f903dd2271f962bae147cbf7
Theo Turner <theo@Theos-MacBook-Pro.local>
2018-10-18 11:14:10 +0100

    Add test JSON object with required properties

--- register_direct/register.py
+++ register_direct/register.py
@@ -1,6 +1,16 @@
 from squid_py.ocean_contracts import OceanContracts
 from squid_py.consumer import register
 
+import json
+
+# Will be posted through requests library
+test_object = {
+    # Required, check full ocean stack for format
+    # Will be web3.toHex-ed
+    "assetId": "",
+}
+test_consumable = json.dumps(test_object)
+
 # OceanContracts is the new name for OceanContractsWrapper
 ocean = OceanContracts(host = 'http://localhost',
                        port = 8545,
@@ -12,4 +22,4 @@ register(publisher_account = ocean.web3.eth.accounts[1],
          provider_account = ocean.web3.eth.accounts[0],
          price = 10,
          ocean_contracts_wrapper = ocean,
-         json_metadata = json_consume)
+         json_metadata = test_consumable)
