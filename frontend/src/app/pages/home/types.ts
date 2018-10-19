b6de29692fcb981816c3828e4ad51d5fbd5721c5
Theo Turner <theo@Theos-MacBook-Pro.local>
2018-10-19 15:14:34 +0100

    Full set of types for publishing an asset

--- frontend/src/app/pages/home/types.ts
+++ frontend/src/app/pages/home/types.ts
@@ -15,8 +15,11 @@ export class OrbitResult {
 }
 
 export class OceanParams {
-    name: string = "H2O clustering"
-
+    name: string = "H2O clustering";
+    description: string = "Clustered OrbitDB data.";
+    contentUrls: string = "http://my.hosted.data";
+    price: number = 10;
+    author: string = "Outlier Ventures";
 }
 
 export class OceanResult {
