88b9c6e40d5e91ce2a5ce991f38dbfa967aaaf49
Theo Turner <theo_turner@outlook.com>
2018-11-30 18:40:19 +0000

    Write publisher address to config file, improve CLI

--- kovan/get_address.js
+++ kovan/get_address.js
@@ -1,7 +1,9 @@
 const HDWalletProvider = require('truffle-hdwallet-provider')
+const fs = require('fs')
 
-provider = new HDWalletProvider(process.env.KOVAN_NMEMORIC, `https://kovan.infura.io/v3/${process.env.INFURA_TOKEN}`)
-
-console.log(provider.addresses[0])
+publisher = new HDWalletProvider(process.env.KOVAN_NMEMORIC, `https://kovan.infura.io/v3/${process.env.INFURA_TOKEN}`)
+address = publisher.addresses[0]
+fs.writeFileSync('address', address);
+console.log("\033[1;32mYour publisher address:\n" + address + "\033[0m")
 
 process.exit()
