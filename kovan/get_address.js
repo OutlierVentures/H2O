8d9af1b8f95a3dee5f3f66febded0ff45d59e0bd
Theo Turner <theo_turner@outlook.com>
2018-12-03 17:59:07 +0000

    Add support for publisher in any wallet e.g. Metamask

--- kovan/get_address.js
+++ kovan/get_address.js
@@ -3,7 +3,9 @@ const fs = require('fs')
 
 publisher = new HDWalletProvider(process.env.KOVAN_NMEMORIC, `https://kovan.infura.io/v3/${process.env.INFURA_TOKEN}`)
 address = publisher.addresses[0]
+key = publisher.wallets[address]._privKey.toString('hex')
 fs.writeFileSync('address', address);
-console.log("\033[1;32mYour publisher address:\n" + address + "\033[0m")
+console.log("\033[0;32mYour publisher address: \033[1;31m" + address + "\n\033[0;32mPrivate key: \033[1;31m" + key +
+"\n\033[1;31mNever share your private key or commit it to Git.\n\033[0;32mAdd this account to a wallet of your choice with the private key, e.g. MetaMask.\033[0m")
 
 process.exit()
