2e065c80ace7e1b8581bb4f1cca4402ba32f54b2
Theo Turner <theo_turner@outlook.com>
2018-12-10 19:50:15 +0000

    Update simple_register to be Kovan and local deployment ambiguous

--- backend/register.py
+++ backend/register.py
@@ -1,21 +1,19 @@
-'''
-This file gives you access to a nice high-level register function for Ocean Protocol:
-simple_register(name, price, description, author, azure_url)
-'''
-
-import squid_py.ocean.ocean as ocean
 from squid_py.ocean.asset import Asset
+import pathlib
 from squid_py.service_agreement.service_factory import ServiceDescriptor
-from unittest.mock import Mock
-
+from squid_py.utils.utilities import generate_new_id
+import logging
+from squid_py.ocean.ocean import Ocean
+from tests.test_utils import get_publisher_ocean_instance, get_consumer_ocean_instance
 
 def simple_register(name, price, description, author, azure_url):
 
+    pub_ocn = get_publisher_ocean_instance()
+    cons_ocn = get_consumer_ocean_instance()
+    logging.debug("".format())
 
-    # Instantiate Ocean, form metadata object and create asset from it
-    ocean_inst = ocean.Ocean('config_local.ini')
-
-    asset_metadata = {
+    # Format metadata as required by Ocean
+    metadata = {
         "base": {
             "name": name,
             "type": "dataset",
@@ -28,58 +26,40 @@ def simple_register(name, price, description, author, azure_url):
         }
     }
 
-    asset = Asset.create_from_metadata(asset_metadata, "http://localhost:5000/api/v1/provider/assets/metadata/")
+    aquarius_acct = pub_ocn.main_account
+    consumer_acct = cons_ocn.main_account
 
-
-    # Set up accounts
-    publisher_address = list(ocean_inst.accounts)[0]
-    consumer_address = list(ocean_inst.accounts)[1]
-    publisher_acct = ocean_inst.accounts[publisher_address]
-    consumer_acct = ocean_inst.accounts[consumer_address]
-
-    # Ensure accounts have sufficient Ocean tokens
-    if publisher_acct.ocean_balance == 0:
-        rcpt = publisher_acct.request_tokens(200)
-        ocean_inst._web3.eth.waitForTransactionReceipt(rcpt)
+    # Ensure there are tokens for the contract
+    if aquarius_acct.ocean_balance == 0:
+        rcpt = aquarius_acct.request_tokens(200)
+        pub_ocn._web3.eth.waitForTransactionReceipt(rcpt)
     if consumer_acct.ocean_balance == 0:
         rcpt = consumer_acct.request_tokens(200)
-        ocean_inst._web3.eth.waitForTransactionReceipt(rcpt)
+        cons_ocn._web3.eth.waitForTransactionReceipt(rcpt)
 
-    # Tokens are required for the transfer
-    assert publisher_acct.ocean_balance > 0
+    assert aquarius_acct.ocean_balance > 0
     assert consumer_acct.ocean_balance > 0
 
+    # If you are hosting Aquarius somewhere else, use the second parameter here
+    asset = Asset.create_from_metadata(metadata, "http://localhost:5000/api/v1/provider/assets/metadata/")
 
-    # Register on blockchain (Keeper)
-    ocean_inst.keeper.market.register_asset(asset, price, publisher_acct.address)
+    # Register asset
+    pub_ocn.keeper.market.register_asset(asset, price, aquarius_acct.address)
 
-    # Assert asset exists and price is correct
-    chain_asset_exists = ocean_inst.keeper.market.check_asset(asset.asset_id)
+    # Check asset exists
+    chain_asset_exists = pub_ocn.keeper.market.check_asset(asset.asset_id)
+    logging.debug("check_asset = {}".format(chain_asset_exists))
     assert chain_asset_exists
-    chain_asset_price = ocean_inst.keeper.market.get_asset_price(asset.asset_id)
-    assert price == chain_asset_price
 
+    # Check price is as sppecified
+    chain_price = pub_ocn.keeper.market.get_asset_price(asset.asset_id)
+    assert price == chain_price
+    logging.debug("chain_price = {}".format(chain_price))
 
-    # Check asset is not already be registered in Aquarius
-    meta_data_assets = ocean_inst.metadata_store.list_assets()
-    
-    if asset.ddo.did in meta_data_assets:
-        print('yer')
-        ocean_inst.metadata_store.get_asset_metadata(asset.ddo.did)
-        ocean_inst.metadata_store.retire_asset_metadata(asset.ddo.did)
-
-
-    # Register asset in Aquarius
-    service_descriptors = [ServiceDescriptor.access_service_descriptor(price, '/purchaseEndpoint', '/serviceEndpoint', 600)]
-    ocean.Client = Mock({'publish_document': '!encrypted_message!'})
-    ocean_inst.register_asset(asset.metadata, publisher_address, service_descriptors)
-
-
-    # Escaped characters are just for coloured prints
     print('\033[0;32mAsset published:')
     print(asset.metadata['base']['name'] + '\033[0m')
 
 
-
+# For quick testing
 if __name__ == "__main__":
     simple_register('test', 10, 'test', 'test', 'test')
